6f17679346c9263a6ceeb54e5992f10a734f7632
zhaofeng <zhaofeng@altamob.com>
2017-01-19 10:22:31 +0800

    test

---
+++ src/test/java/com/zf/util/StringTest.java
@@ -0,0 +1,25 @@
+package com.zf.util;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// ios:
+		// 200*200
+		// 210*140
+		// 250*200
+		// 250*250
+		// 532*108
+		// 600*400
+		// 640*100
+		// 640*120
+		// 640*160
+		// 640*240
+		// 640x744
+		// 640x920
+		// 640*1136
+		// 720x1038
+		//
+		//
+		// android:
+	}
+}
