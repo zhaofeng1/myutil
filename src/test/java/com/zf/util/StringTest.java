07c271731fb3fb587e9e2f8e12a95f22d08c6aff
zhaofeng <zhaofeng@altamob.com>
2017-03-30 14:10:28 +0800

    test

--- src/test/java/com/zf/util/StringTest.java
+++ src/test/java/com/zf/util/StringTest.java
@@ -62,4 +62,11 @@ public class StringTest {
 	public void testChinese() {
 
 	}
+
+	@Test
+	public void testLog() {
+		String s = "{\"platform\":\"android\",\"app_version\":\"1.7.4\",\"robotId\":\"79818082567d86fe\",\"trackingUrl\":\"http://app.appsflyer.com/id.co.babe?pid=appcoachs_int&c=Appcoach250_21069&clickid=fc-00051CA43281A1490142872388316&android_id=&advertising_id=&af_enc_data=pqEQ7Yves8DpTKDGKVVHX2bgfPUrhZ5frgqYqeNPqwCvlkVUN7ioQXkyUfljIt1Usxz0kRkO%0AVTIQfVJmlYfBjg%3D%3D%0A\",\"sdk_version\":\"1.3\",\"clickId\":\"130151023010154221490142855390\",\"referer\":\"af_tranid=YFxrayrF2pA6uOKkkFDyqg&pid=appcoachs_int&c=Appcoach250_21069&clickid=fc-00051CA43281A1490142872388316&af_enc_data=pqEQ7Yves8DpTKDGKVVHX2bgfPUrhZ5frgqYqeNPqwCvlkVUN7ioQXkyUfljIt1Usxz0kRkO\nVTIQfVJmlYfBjg==\n&utm_source=appcoachs_int&utm_campaign=Appcoach250_21069\",\"clickResult\":\"1\",\"finalUrl\":\"https://play.google.com/store/apps/details?id=id.co.babe&referrer=af_tranid=YFxrayrF2pA6uOKkkFDyqg&pid=appcoachs_int&c=Appcoach250_21069&clickid=fc-00051CA43281A1490142872388316&af_enc_data=pqEQ7Yves8DpTKDGKVVHX2bgfPUrhZ5frgqYqeNPqwCvlkVUN7ioQXkyUfljIt1Usxz0kRkO\nVTIQfVJmlYfBjg==\n&utm_source=appcoachs_int&utm_campaign=Appcoach250_21069\",\"app_name\":\"com.wifiup\"}";
+		System.out.println(s.replace("\n", ""));
+	}
+
 }
