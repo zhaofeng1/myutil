4d88937f8b6a9e8e3ae5025c45c5922348a4f687
dell <dell@DESKTOP-ABEPJTS>
2017-08-19 10:34:44 +0800

    fix

--- src/test/java/com/zf/util/StringTest.java
+++ src/test/java/com/zf/util/StringTest.java
@@ -1,6 +1,11 @@
 package com.zf.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -96,5 +101,72 @@ public class StringTest {
 			payout = payout.replace(str, "");
 		}
 		System.out.println(payout);
+	}
+
+	@Test
+	public void test3() {
+		String url = "http://apptrknow.com/dir/redirect?c=559&offerId=20331830&affid=345&cid=[cid]&data1=&data2=[data2]&data3=[data3]&data4=[data4]&affsub1=[affsub1]&device_id=[device_id]&idfa=[idfa]&gaid=[gaid]";
+		if (url.contains("?")) {
+			int qIndex = url.indexOf("?") + 1;
+			String tempUrl = url.substring(0, qIndex);
+			System.out.println(tempUrl);
+			String param = url.substring(qIndex);
+			System.out.println(param);
+
+			Map<String, String> paramMap = parseMap(param);
+
+			List<String> list = new ArrayList<String>();
+			list.add("cid");
+			list.add("data1");
+			list.add("data2");
+			list.add("data3");
+			list.add("data4");
+			list.add("affsub1");
+			list.add("device_id");
+			list.add("idfa");
+			list.add("gaid");
+
+			for (String s : list) {
+				if (paramMap.containsKey(s)) {
+					paramMap.remove(s);
+				}
+			}
+
+		}
+	}
+
+	public Map<String, String> parseMap(String param) {
+		Map<String, String> map = new HashMap<String, String>();
+		String[] array = param.split("&");
+		for (String s : array) {
+			String[] keyvalue = s.split("=");
+			if (keyvalue.length == 2) {
+				map.put(keyvalue[0], keyvalue[1] == null ? "" : keyvalue[1]);
+			} else if (keyvalue.length == 1) {
+				map.put(keyvalue[0], "");
+			}
+		}
+		System.out.println(JSON.toJSONString(map));
+		return map;
+	}
+
+	@Test
+	public void testUrl() throws MalformedURLException {
+		String clickUrl = "http://52.77.99.53/acs.php?sid=106157&adid=6120174";
+		URL clickURL = new URL(clickUrl);
+		String domain = clickURL.getHost();
+		System.out.println(domain);
+	}
+
+	@Test
+	public void testBoolean() {
+
+		List<String> list = new ArrayList<String>();
+		handle(list);
+		System.out.println(list.size());
+	}
+
+	private void handle(List<String> list) {
+		list.add("a");
 	}
 }
